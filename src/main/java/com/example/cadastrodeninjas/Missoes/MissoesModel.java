9a139eadff4e53462dfe0146b753a7c09b8d8ef6
jamesmayke <jamezmayke28@gmail.com>
2025-07-14 14:41:46 -0300

    Package de missoes criado, classe de missoes criado

--- src/main/java/com/example/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/com/example/cadastrodeninjas/Missoes/MissoesModel.java
@@ -1,2 +1,19 @@
-package com.example.cadastrodeninjas.Missoes;public class MissoesModel {
+package com.example.cadastrodeninjas.Missoes;
+
+import com.example.cadastrodeninjas.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private NinjaModel ninja;
+
 }
