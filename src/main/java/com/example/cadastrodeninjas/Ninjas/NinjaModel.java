b190ffc9ae93c35ae8d712562703e44df22dbf73
jamesmayke <jamezmayke28@gmail.com>
2025-07-21 13:34:24 -0300

    Adição banco de dados + ajustes

--- src/main/java/com/example/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/com/example/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -2,52 +2,30 @@ package com.example.cadastrodeninjas.Ninjas;
 
 import com.example.cadastrodeninjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
-    @ManyToOne // Um ninja tem uma unica missao
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-    public NinjaModel() {}
-
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
