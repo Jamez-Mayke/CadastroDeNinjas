d1ec4c2b2e16b11424d394a03648e70e1fa07f38
jamesmayke <jamezmayke28@gmail.com>
2026-03-16 21:05:53 -0300

    first commit

---
+++ src/main/java/br/com/jamezmayke/CadastroDeNinjas/Application.java
@@ -0,0 +1,13 @@
+package br.com.jamezmayke.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
